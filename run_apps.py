43d2403594bbc66d08c4617627add1a99f8d52a3
DavidContrerasFHMM <david.contreras@fhmm.org>
2025-04-10 15:35:58 -0600

    Actualización de URL

--- run_apps.py
+++ run_apps.py
@@ -4,7 +4,7 @@ import subprocess
 flask_command = "cmd /k python mainapp.py"
 
 # Command to run ngrok
-ngrok_command = "cmd /k ngrok http --url=superb-monarch-charmed.ngrok-free.app 5000"
+ngrok_command = "cmd /k ngrok http --url=talented-drake-gratefully.ngrok-free.app 5000"
 
 # Open a new command prompt window to run the Flask app
 subprocess.Popen(['start', 'cmd', '/k', flask_command], shell=True)
